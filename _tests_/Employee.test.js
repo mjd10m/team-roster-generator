aa96a24e214b141a4e3c921bd9c27a6c66761cd8
mjd10m <mjd10m@outlook.com>
2022-08-21 19:17:03 -0400

    initial commit plus classes

---
+++ _tests_/Employee.test.js
@@ -0,0 +1,27 @@
+const Employee = require('../lib/Employee')
+
+test('creates employee object', () => {
+    const employee = new Employee('Dave', '1', 'dave1@gmail.com');
+
+    expect(employee.name).toBe('Dave');
+    expect(employee.id).toBe('1');
+    expect(employee.email).toBe('dave1@gmail.com');
+});
+
+test('gets employee name from Employee object', () => {
+    const employee = new Employee('Dave', '1', 'dave1@gmail.com');
+
+    expect(employee.getName()).toBe(employee.name);
+});
+
+test('gets employee id from Employee object', () => {
+    const employee = new Employee('Dave', '1', 'dave1@gmail.com');
+
+    expect(employee.getId()).toBe(employee.id);
+});
+
+test('gets employee email from Employee object', () => {
+    const employee = new Employee('Dave', '1', 'dave1@gmail.com');
+
+    expect(employee.getEmail()).toBe(employee.email);
+});
